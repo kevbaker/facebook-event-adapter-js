0cbf708cbf2065dcd353366edf68dafbf549c787
Kevin Baker <kbaker@kev.io>
2014-07-30 17:57:26 -0700

    added facebook login form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,7 @@ angular.module('starter.controllers', [])
   $scope.loginData = {};
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
+  $ionicModal.fromTemplateUrl('templates/facebook_login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
@@ -23,6 +23,17 @@ angular.module('starter.controllers', [])
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doFacebookLogin = function() {
     console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
