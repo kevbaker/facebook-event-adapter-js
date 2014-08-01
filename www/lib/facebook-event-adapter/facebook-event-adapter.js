fec443c993cb08ff65d16ea356deee84e4fdb97d
Kevin Baker <kbaker@kev.io>
2014-08-01 01:52:14 -0700

    moving adapter around for code cleanup

--- www/lib/facebook-event-adapter/facebook-event-adapter.js
+++ www/lib/facebook-event-adapter/facebook-event-adapter.js
@@ -5,12 +5,15 @@
  * FacebookConnectPlugin. Each FacebookConnectPlugin method
  * will broadcast an event with the asynchronous response.
  *
+ * TODO: This is a working prototype. This code should be moved
+ * into a module to follow best practices for library distribution.
  */
 
 
 // Login
 window.loginFacebook = function () {
-  facebookConnectPlugin.login( ["email","user_about_me","user_location"],
+  var permissions = ["email","user_about_me","user_location"];
+  facebookConnectPlugin.login( permissions,
     function (response) {
       var event = new CustomEvent("FACEBOOK_LOGIN_COMPLETE",
         { detail: { response: response },
